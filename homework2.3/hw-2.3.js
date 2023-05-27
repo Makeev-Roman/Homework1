ac9cd41a6bb002a0dfb993a919db6e76d0bcef1f
Makeev-Roman <makrom.one@gmail.com>
2023-05-27 17:32:23 +0300

    сделал домашку 2.3

---
+++ homework2.3/hw-2.3.js
@@ -0,0 +1,75 @@
+// //задание 1.
+// let pasword = 'невероятносложнаяизашифрованнаякомбинацияцифрибукв';
+// let inputPasword = prompt(`введите пароль`);
+// if (inputPasword === pasword) {
+//     alert('Пароль введен верно');
+// } 
+// else {alert('Пароль введен неправильно');
+// }
+// //задание 2.
+// let c = 0;
+// if (c>0 && c<10) {
+//     alert('Верно')
+// } else {
+//     alert('Неверно')
+// }
+//Задание 3.
+// let d = 99;
+// let e = 101;
+// if (d>100 || e>100) {
+//     alert('Верно')
+    
+// } else {
+//     alert('Неверно')
+// }
+// //Задание 4.
+// let a = '2';
+// let b = '3';
+// alert(Number(a) + Number(b));
+//Задание 5. 
+// let monthNumber = prompt(`Введите номер месяца от 1 до 12`);
+// if (monthNumber>12 || monthNumber<1) {alert('неверный месяц')
+    
+// } else {
+//     switch (monthNumber) {
+//         case '1':
+//             alert('Зимний месяц')        
+//             break;
+//         case '2':
+//             alert('Зимний месяц')        
+//             break;
+//         case '3':
+//             alert('Весенний месяц')        
+//             break;
+//         case '4':
+//             alert('Весенний месяц')        
+//             break;
+//         case '5':
+//             alert('Весенний месяц')        
+//             break;
+//         case '6':
+//                 alert('Летний месяц')        
+//             break;  
+//         case '7':
+//                 alert('Летний месяц')        
+//             break;
+//         case '8':
+//                 alert('Летний месяц')        
+//             break;
+//         case '9':
+//                 alert('Осенний месяц')        
+//             break;  
+//         case '10':
+//                 alert('Осенний месяц')        
+//             break; 
+//         case '11':
+//                 alert('Осенний месяц')        
+//             break; 
+//         case '12':
+//                 alert('Зимний месяц')        
+//             break; 
+//         default: 
+            
+//             break;
+//     }
+// }
